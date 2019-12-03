0ac4c9d345f294e2be92f320282263a4055a57de
Danny Tseng <dtfocus1025@gmail.com>
2019-12-03 15:47:51 -0800

    changed center photo

--- index.js
+++ index.js
@@ -4,11 +4,24 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const slider = document.querySelector(".slider");
     const hero = document.querySelector(".hero");
+    const logo = document.querySelector(".logo");
+    const menu = document.querySelector(".menu");
 
     const tl = new TimelineMax();
 
 
-    tl.fromTo(hero, 1, {height: "0%"}, {height: "80%"});
+    tl.fromTo(
+        hero, 1, { height: "0%" }, { height: "80%", ease: Power2.easeInOut }
+    ).fromTo(
+        hero, 1.2, { width: "100%" }, { width: "80%", ease: Power2.easeInOut }
+    ).fromTo(
+        slider, 1.2, { x: "-100%" }, { x: "0%", ease: Power2.easeInOut },
+        "-=1.2"
+    ).fromTo(
+        logo, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=0.5"
+    ).fromTo(
+        menu, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=0.5"
+    )
 
 
 })
