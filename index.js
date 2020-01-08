85ba3765e669413d17aaab94a31857dbe7d6a2d3
Danny Tseng <dtfocus1025@gmail.com>
2020-01-07 23:31:10 -0800

    adjusted selfie background, hamburger menu and the white space on the right of the site in mobile view

--- index.js
+++ index.js
@@ -33,20 +33,21 @@ const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li')
+    const navbarLinks = document.getElementsByClassName('navbar-links')[0];
 
     
     burger.addEventListener('click', () => {
         //Toggle Nav
-        nav.classList.toggle('nav-active');
-
+        // nav.classList.toggle('nav-active');
+        navbarLinks.classList.toggle('active');
         //Animate Links
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = '';
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`
-            }
-        })
+        // navLinks.forEach((link, index) => {
+        //     if (link.style.animation) {
+        //         link.style.animation = '';
+        //     } else {
+        //         link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`
+        //     }
+        // })
 
         //Burger Animation
         burger.classList.toggle('toggle');
